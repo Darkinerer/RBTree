3a4ea6054a55613fc1ce778f4e0b81eb57d41e25
Darkinerer <56995220+Darkinerer@users.noreply.github.com>
2022-04-20 15:31:55 +0300

    RBTree

---
+++ RBT/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include"RBTree.h"
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+	vector<int> nums{ 10,40,30,60,90,70,20,50,80,100 };
+	RBTree<int> tree;
+	for (auto num : nums)
+		tree.insert(num);
+	cout << "Прямий обхід" << endl;
+	tree.preOrder();//прямий 
+	cout<<endl <<"Серединний" << endl;
+	tree.inOrder();//серединний
+	cout<<endl  <<	"Зворотній" << endl;
+	tree.postOrder();//зворотній
+	cout << endl;
+	cout << "Знайти вузол 30: \ n";
+	cout << endl << tree.search(30)->key << endl;
+	cout << "Видалити вузол з ключем 100 \ n"<< endl;
+	tree.remove(100);
+	tree.preOrder();
+	cout << endl;
+	cout << "\n Деталі дерева: \ n";
+	tree.print();
+	cin.get();
+	return 0;
+}
+
